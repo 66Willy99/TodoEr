07343aaaf1f6a24f82a3257aa7c77615e87fdddc
Willy <guillermo.mercado26@gmail.com>
2024-09-24 00:24:43 -0300

    update - delete methods and completed todo

--- todo/todo.py
+++ todo/todo.py
@@ -11,7 +11,11 @@ bp=Blueprint('todo', __name__)
 def index():
     db, c = getDb()
     c.execute(
-        'SELECT t.id, t.description, u.username, t.completed, t.created_at FROM todo t JOIN user u ON t.created_by = u.id ORDER BY t.created_at DESC'
+        'SELECT t.id, t.description, u.username, t.completed, t.created_at '
+        'FROM todo t JOIN user u ON t.created_by = u.id '
+        'WHERE t.created_by = %s '
+        'ORDER BY t.created_at DESC'
+        , (g.user['id'],)
     )
     todos = c.fetchall()
     return render_template('todo/index.html', todos=todos)
@@ -38,7 +42,46 @@ def create():
         
     return render_template('todo/create.html')
 
+
+def getTodoById(id):
+    db, c = getDb()
+    c.execute(
+        'SELECT t.id, t.description, t.completed, t.created_at, u.username'
+        ' FROM todo t JOIN user u ON t.created_by = u.id WHERE t.id = %s',
+        (id,)
+    )
+    todo = c.fetchone()
+    if todo is None:
+        abort(404, f"Todo id {0} doesn't exist.".format(id))
+    return todo
+
 @bp.route('/<int:id>/update', methods=['GET','POST'])
 @loginRequired
-def update():
-    return render_template('todo/update.html')
+def update(id):
+    todo = getTodoById(id)
+    if request.method == 'POST':
+        description = request.form['description']
+        completed = True if request.form.get('completed') == 'on' else False
+        error = None
+        if not description:
+            error = 'Description is required.'
+        if error is not None:
+            flash(error)
+        else:
+            db, c = getDb()
+            c.execute(
+                'UPDATE todo SET description = %s, completed = %s'
+                ' WHERE id = %s and created_by = %s',
+                (description, completed, id, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('todo.index'))
+    return render_template('todo/update.html', todo=todo)
+
+@bp.route('/<int:id>/delete', methods=['POST'])
+@loginRequired
+def delete(id):
+    db, c = getDb()
+    c.execute('DELETE FROM todo WHERE id = %s and created_by= %s', (id, g.user['id']))
+    db.commit()
+    return redirect(url_for('todo.index'))
